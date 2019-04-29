df021d93c1cca6ec5f800a28954328f0a08a02f4
rogong <itingasng@gmail.com>
2019-04-29 16:15:44 +0100

    signup complete

--- server.js
+++ server.js
@@ -2,12 +2,25 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 
 
 const app = express();
 
 const dbConfig = require('./config/secret');
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+
+    res.header("Access-Controller-Allow-Methods", "OPTIONS, HEAD, PUT, GET, POST, PATCH, DELETE");
+
+    next();
+});
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
